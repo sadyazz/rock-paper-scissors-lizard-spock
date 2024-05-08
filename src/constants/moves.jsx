03fe2692979feebd101f3f1e9843370a30734967
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-08 16:38:16 +0200

    added moves.jsx

---
+++ src/constants/moves.jsx
@@ -0,0 +1,49 @@
+const moves = [
+    {
+      title: "rock",
+      icon: (
+        <div className="bg-white hover:bg-slate-300 rounded-full h-28 w-28 flex items-center justify-center p-4 m-5 hover:cursor-pointer">
+          {" "}
+          <img src="src\assets\rock.svg"></img>
+        </div>
+      ),
+    },
+    {
+      title: "paper",
+      icon: (
+        <div className="bg-white hover:bg-slate-300 rounded-full h-28 w-28 flex items-center justify-center p-4 m-5 hover:cursor-pointer">
+          {" "}
+          <img src="src\assets\paper.svg"></img>
+        </div>
+      ),
+    },
+    {
+      title: "scissors",
+      icon: (
+        <div className="bg-white hover:bg-slate-300 rounded-full h-28 w-28 flex items-center justify-center p-4 m-5 hover:cursor-pointer">
+          {" "}
+          <img src="src\assets\scissors.svg"></img>
+        </div>
+      ),
+    },
+    {
+      title: "lizard",
+      icon: (
+        <div className="bg-white hover:bg-slate-300 rounded-full h-28 w-28 flex items-center justify-center p-4 m-5 hover:cursor-pointer">
+          {" "}
+          <img src="src\assets\lizard.svg"></img>
+        </div>
+      ),
+    },
+    {
+      title: "spock",
+      icon: (
+        <div className="bg-white hover:bg-slate-300 rounded-full h-28 w-28 flex items-center justify-center p-4 m-5 hover:cursor-pointer">
+          {" "}
+          <img src="src\assets\spock.svg"></img>
+        </div>
+      ),
+    },
+  ];
+
+  export { moves };
