eb719e2f4539ae5f137aab2ce916ebfaf7ca9d71
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-12 10:48:50 +0200

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,24 @@ import Rules from './components/Rules'
 import Score from './components/Score'
 
 function App() {
-  const [userChoice, setUserChoice] = useState("");
-  const [computerChoice, setcomputerChoice] = useState("");
+ 
   const [score, setScore] = useState(0);
   const [computerScore, setComputerScore] = useState(0);
-  const [turnResult, setTurnResult] = useState(null);
-  const [result, setResult] = useState('Let\'s see who wins');
-  const [gameOver, setGameOver] = useState(false);
 
-  const choice = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+  // const handleScoreChange = (newScore, newComputerScore) => {
+  //   setScore(newScore);
+  //   setComputerScore(newComputerScore);
+  // };
   
   return (
    <div className='bg-sky-950 w-screen h-screen'>
-    <Header className=" top-0 x-10"/>
-    <Score score={score} computerScore={computerScore}  className=""/>
-    <Game/>
-    <Rules className=" flex justify-end lg:justify-end relative z-50"/>
+    <Header text='rock paper scissors lizard spock'/>
+    <Score  score={score} computerScore={computerScore} />
+    <Game
+      //handleScoreChange={handleScoreChange} 
+      score ={score} setScore={setScore} computerScore = {computerScore} setComputerScore = {setComputerScore}
+     />
+    <Rules/>
    </div>
   )
 }
