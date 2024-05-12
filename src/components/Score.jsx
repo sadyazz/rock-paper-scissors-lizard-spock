1e5ddf6de6b32e6f1f5cc4845a895a6836c7a081
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-12 10:47:53 +0200

    updated Score.jsx

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useState, useEffect } from 'react'
 
 const Score = ({score, computerScore}) =>{
+ 
+  const [isDifferent, setIsDifferent] = useState(false);
+
+useEffect(() => {
+  if (score > computerScore) {
+    setIsDifferent("border-green-400");
+  } else if (score < computerScore) {
+    setIsDifferent("border-red-400");
+  } else {
+    setIsDifferent("");
+  }
+}, [score, computerScore]);
+
+const borderClass = isDifferent;
+  
     return(
-        <div className="flex justify-evenly rounded-xl border-slate-400 border-2 text-gray mt-5 text-4xl  py-5 w-1/4 h-min m-auto">
-        <p className="text-slate-200">user {score}</p>
-      <p className="text-slate-200">{computerScore} computer</p>
+        <div className={`flex justify-evenly rounded-xl border-slate-400 border-2 text-gray my-5 text-4xl  py-5 w-1/4 h-min m-auto  ${borderClass}`}>
+        <p className="text-slate-200">{score} : {computerScore}</p>
         </div>
     )
 }
