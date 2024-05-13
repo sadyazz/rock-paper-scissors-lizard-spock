a2afd03bb234064c044fdde4aceb6b12b2bdd9de
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-13 12:08:51 +0200

    updated Game.jsx

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,60 +1,27 @@
 import React from "react";
 import { useState, useEffect } from 'react'
 import { moves } from "../constants/moves";
-import Score from "./Score";
 
+const Game = ({ score, setScore, computerScore, setComputerScore }) => {
 
-import { useMemo } from "react";
-import { useRef } from "react";
-//{ handleScoreChange }
-const Game = ({score, setScore, computerScore, setComputerScore}) =>{
+  const [selected, setSelected] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
 
-    const [selected, setSelected] = useState(null);
-    const [computerChoice, setComputerChoice] = useState(null);
-    // const [score, setScore] = useState(0);
-    // const [computerScore, setComputerScore] = useState(0);
+  const [resultMessage, setResultMessage] = useState("");
 
-//     useEffect(() =>{
-//         const comboMoves = selected + computerChoice
-//         if( comboMoves === 'paperrock' || comboMoves === 'rocklizard' || comboMoves === 'lizardspock' || comboMoves === 'spockscissors' || comboMoves === 'scissorspaper' || comboMoves === 'rockscissors' || comboMoves === 'scissorslizard' || comboMoves === 'lizardpaper' || comboMoves === 'paperspock' || comboMoves === 'spockrock'){
-//             console.log(comboMoves + "WON")
-            
-//         }else if(selected === computerChoice){
-//             console.log(comboMoves +" TIED")    
-//         }else
-//         console.log(comboMoves + "LOST")
-       
-        
-//    })
-
-//    const handlePlayerChoice = (playerChoice) => {
-//     const choices = ["rock", "paper", "scissors", "lizard", "spock"];
-//     const randomIndex = Math.floor(Math.random() * choices.length);
-//     const randomChoice = choices[randomIndex];
-//     setComputerChoice(randomChoice);
-//     setSelected(playerChoice);
-
-//     console.log("User choice:", playerChoice);
-//   console.log("Computer choice:", randomChoice);
-//   };
-const [resultMessage, setResultMessage] = useState("");
-
-  
   useEffect(() => {
     const comboMoves = selected + computerChoice
-   
+
     if (!!selected && !!computerChoice) {
       if (selected === computerChoice) {
         setResultMessage("Tied");
-      } else if( comboMoves === 'paperrock' || comboMoves === 'rocklizard' || comboMoves === 'lizardspock' || comboMoves === 'spockscissors' || comboMoves === 'scissorspaper' || comboMoves === 'rockscissors' || comboMoves === 'scissorslizard' || comboMoves === 'lizardpaper' || comboMoves === 'paperspock' || comboMoves === 'spockrock'){
-        setScore(score+1);
-        console.log(score);
+      } else if (comboMoves === 'paperrock' || comboMoves === 'rocklizard' || comboMoves === 'lizardspock' || comboMoves === 'spockscissors' || comboMoves === 'scissorspaper' || comboMoves === 'rockscissors' || comboMoves === 'scissorslizard' || comboMoves === 'lizardpaper' || comboMoves === 'paperspock' || comboMoves === 'spockrock') {
+        setScore(score + 1);
         setResultMessage("You won");
-      }else {
-        setComputerScore(computerScore+1);
-        console.log(computerScore);
+      } else {
+        setComputerScore(computerScore + 1);
         setResultMessage("You lost");
-    }
+      }
     }
   }, [selected, computerChoice]);
 
@@ -66,26 +33,26 @@ const [resultMessage, setResultMessage] = useState("");
     setSelected(playerChoice);
   };
 
-      return (
-        <div className=" text-gray m-0 text-4xl w-screen flex justify-center flex-wrap py-7">
-          
-          <p className="w-screen flex justify-center text-white font-medium">{resultMessage}</p>
+  return (
+    <div className=" text-gray m-0 text-4xl w-screen flex justify-center flex-wrap py-7">
 
-          {moves.map(({ title, icon }) => {
-            return (
-              <div
-                key={title}
-                onClick={() => {
-                  handlePlayerChoice(title);
-                }}
-                className="block mt-4"
-              >
-                {icon}
-              </div>
-            );
-          })}
-        </div>
-    )
+      <p className="w-screen flex justify-center text-white font-medium">{resultMessage}</p>
+
+      {moves.map(({ title, icon }) => {
+        return (
+          <div
+            key={title}
+            onClick={() => {
+              handlePlayerChoice(title);
+            }}
+            className="block mt-4"
+          >
+            {icon}
+          </div>
+        );
+      })}
+    </div>
+  )
 }
 
 export default Game
